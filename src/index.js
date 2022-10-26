eab4f884e06f5d9e26cb44db0df17f43b4b84959
adrianapvent <adriana.e.pena@gmail.com>
2022-10-25 19:27:45 -0500

    logo now populates! yay

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = document.getElementById('root');
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
